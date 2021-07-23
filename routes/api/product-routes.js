4e382bc41a27e4af1a876c98ed9819930dacb23f
tannerlsmith <tanner.smith@uvm.edu>
2021-07-23 11:41:58 -0400

    added models, associations

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,6 +7,19 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      Category, 
+      {
+        model: Tag, 
+        throught: ProductTag
+      }
+    ]
+  }).then(data => {
+    res.json(data)
+  }).catch(err => {
+    res.status(500).json(err)
+  })
 });
 
 // get one product
